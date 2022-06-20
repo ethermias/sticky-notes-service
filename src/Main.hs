9b36bb3bc0b429ac9fcb2cf270ebd88b5f7c4853
jermisha <ermias.shiferaw@gmail.com>
2022-06-20 10:10:35 -0400

    table of content is added

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,17 @@ module Main where
 
 import Web.Scotty
 import Routes
+import System.IO
+import Content
 
 main :: IO ()
 main = do
-  putStrLn "Starting Server..."
-  scotty 8888 routes 
+  putStrLn "Starting Server..."  
+  name <- tableOfContents
+  scotty 8888 $ routes name 
+
+tableOfContents :: IO String
+tableOfContents = do 
+                handle <- openFile "tableOfContents.yaml" ReadMode  
+                contents <- hGetContents handle 
+                return contents 
--- src/Routes.hs
+++ src/Routes.hs
@@ -3,15 +3,15 @@ module Routes where
 import Web.Scotty
 import Content
 import Swagger
+import qualified Data.Text.Lazy as T
  
-routes :: ScottyM ()
-routes = do
-  get "/swagger" $ do
-    json allSwaggers
-  get "/version" $ do
-    text "0.0.1"
-  get "/contents" $ do
-    json allContents
+routes :: String -> ScottyM ()
+routes name = do
+  get "/" $ text $ T.pack name
+  get "/swagger" $ json allSwaggers
+  get "/version" $ text "0.0.1"
+  get "/contents" $ json allContents
   get "/contents/:contId" $ do
     contId <- param "contId"
     json (filter (matchesId contId) allContents)  
+  
